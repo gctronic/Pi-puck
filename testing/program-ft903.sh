e88959b8b038d3388df7a33eec56b6f9699976a0
GCtronic <stefano@gctronic.com>
2019-05-22 10:23:04 +0200

    Added reset of the FT903 after programming

--- testing/program-ft903.sh
+++ testing/program-ft903.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
 
+# Program the firmware
 sudo dfu-util -d 0403:0fde -D /home/pi/Pi-puck/FT903/Pi-puck_FT903_Firmware_DFU.bin
 
+# Reset FT903
+echo Resetting FT903...
+gpio write 27 0
+sleep 0.1
+gpio write 27 1
 
+# Wait for 3 seconds and the USB camera device should be back
+sleep 3
+echo Done.
 
