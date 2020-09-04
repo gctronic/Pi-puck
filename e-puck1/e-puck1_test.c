1504ad09ef6ab9ee8e2184826494d4ae643d0b0e
gctronic <stefano@gctronic.com>
2020-09-04 04:55:20 +0000

    updated e-puck1 test example to exploit i2c restart

--- e-puck1/e-puck1_test.c
+++ e-puck1/e-puck1_test.c
@@ -10,7 +10,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-#define ROBOT_ADDRESS 0x1F
+#define ROBOT_ADDR 0x1F
 
 #define RING_LEDS		0x00
 #define FRONT_BODY_LEDS	0x01
@@ -42,34 +42,57 @@ uint16_t prox[8];
 uint16_t prox_amb[8];
 int16_t mot_steps[2];
 uint8_t temp_write[32];
-uint8_t temp_read[64];
+uint8_t temp_read[32];
 
-
-int read_reg(int file, uint8_t reg, int count, uint8_t *data) {
-
-	if(write(file, &reg, 1) != 1) {
-		perror("robot i2c read error 1");
-		return -1;
-	}
-
-	if(read(file, data, count) != count) {
-		perror("robot i2c read error 2");
+int write_reg(uint8_t reg, int count, uint8_t *data) {
+	temp_write[0] = reg;
+	memcpy(&temp_write[1], data, count);
+	
+	if(write(fh, temp_write, count+1) != (count+1)) {
+		perror("write_reg");
 		return -1;
 	}
 
 	return 0;
 }
 
-int write_reg(int file, uint8_t reg, int count, uint8_t *data) {
-	temp_write[0] = reg;
-	memcpy(&temp_write[1], data, count);
+// The following implementation uses a "restart" between a "write" and "read".
+int read_reg(uint8_t reg, int count, uint8_t *data) {
 	
-	if(write(file, temp_write, count+1) != (count+1)) {
-		perror("robot i2c write error");
-		return -1;
+    struct i2c_rdwr_ioctl_data packets;
+    struct i2c_msg messages[2];
+	int trials = 0;
+	
+	// S Addr Wr [A] Data(actuators) NA Sr Addr Rd [A] Data(sensors) [A] P
+    messages[0].addr  = ROBOT_ADDR;
+    messages[0].flags = 0;
+    messages[0].len   = 1;
+    messages[0].buf   = &reg;
+	
+    messages[1].addr  = ROBOT_ADDR;
+    messages[1].flags = I2C_M_RD;
+    messages[1].len   = count;
+    messages[1].buf   = data;
+
+    packets.msgs      = messages;
+    packets.nmsgs     = 2;
+	
+	// Form the tests it was noticed that sometimes (about 1/1000) the communication give a "timeout error" followed by "remote I/O" error.
+	// Thus 3 retrials are done in case of errors.
+	while(trials < 3) {
+		if(ioctl(fh, I2C_RDWR, &packets) < 0) {		
+			trials++;
+			continue;
+		}
+		break;
 	}
 
-	return 0;
+	if(trials > 2) {
+		perror("read_reg: ");
+		return -1;
+	} else {
+		return 0;
+	}
 }
 
 int main() {
@@ -87,30 +110,30 @@ int main() {
 	
 	fh = open("/dev/i2c-4", O_RDWR);	// open the I2C dev driver for bus 4
 
-	ioctl(fh, I2C_SLAVE, ROBOT_ADDRESS);
+	ioctl(fh, I2C_SLAVE, ROBOT_ADDR);
 
 	data = 0x01;
-	write_reg(fh, IR_ENABLE, 1, &data);
+	write_reg(IR_ENABLE, 1, &data);
 
 	while(1) {
 
-		read_reg(fh, LEFT_STEPS, 4, temp_read);
+		read_reg(LEFT_STEPS, 4, temp_read);
 		mot_steps[0] = temp_read[1]*256 + temp_read[0];
 		mot_steps[1] = temp_read[3]*256 + temp_read[2];		
 		printf("steps: %.4d, %.4d\r\n", mot_steps[0], mot_steps[1]);		
 		
 		for(i=0; i<8; i++) {
-			read_reg(fh, PROX_SENSOR0+i, 2, &prox[i]);
-			read_reg(fh, AMBIENT_SENSOR0+i, 2, &prox_amb[i]);
+			read_reg(PROX_SENSOR0+i, 2, (uint8_t*)&prox[i]);
+			read_reg(AMBIENT_SENSOR0+i, 2, (uint8_t*)&prox_amb[i]);
 		}
 		printf("prox: %.4d, %.4d, %.4d, %.4d, %.4d, %.4d, %.4d, %.4d\r\n", prox[0], prox[1], prox[2], prox[3], prox[4], prox[5], prox[6], prox[7]);
 		printf("ambient: %.4d, %.4d, %.4d, %.4d, %.4d, %.4d, %.4d, %.4d\r\n\n", prox_amb[0], prox_amb[1], prox_amb[2], prox_amb[3], prox_amb[4], prox_amb[5], prox_amb[6], prox_amb[7]);		
 		
  		data = 0x55;
-		write_reg(fh, RING_LEDS, 1, &data);
+		write_reg(RING_LEDS, 1, &data);
 		usleep(200000);
 		data = 0xAA;
-		write_reg(fh, RING_LEDS, 1, &data);
+		write_reg(RING_LEDS, 1, &data);
 		usleep(200000);
 
 	}
